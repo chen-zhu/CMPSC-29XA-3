c96bd65587b298972ef1bd0563d50bdd22546b2c
sarawang <369278452@qq.com>
2019-10-25 17:14:47 -0700

    finish frontend

---
+++ frontend/src/.eslintrc.js
@@ -0,0 +1,7 @@
+ module.exports = {
+     "extends": "react-app",
+     "rules" : {
+     	"eqeqeq": "off",
+     	"no-unused-vars": "off"
+     }
+ }
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ChatService from '../services/ChatService'
 
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
 class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      url: "https://chat.cs291.com",
+      url: "http://chat.cs291.com",
       username: "test",
       password: "test"
     };
@@ -14,45 +19,73 @@ class Login extends React.Component {
   onSubmit = event => {
     event.preventDefault();
     console.log('submit')
-    ChatService.login(this.state.url, this.state.username, this.state.password)
-    // this.setState({ status: `Result: ${this.state.lhs + this.state.rhs}` });
-    // this.props.updateCounter();
+    ChatService.login(this.state.url, this.state.username, this.state.password).then(
+      res => {
+        this.props.afterLogin(true)
+      },
+      err => {
+        alert(err)
+      }
+    )
   };
 
   onChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
+    console.log(event)
+    this.setState({ [event.target.id]: event.target.value });
   };
 
   render() {
     return (
-      <div> 
-        <div className = "title">please login</div>
-        <form onSubmit={this.onSubmit}>
-          <p>server url:</p>
-          <input 
-            name="url"
-            type="text"
-            onChange={this.onChange}
-            value={this.state.url}
-          />
-          <p>username:</p>
-          <input 
-            name="username"
-            type="text"
-            onChange={this.onChange}
-            value={this.state.username}
-          />
-          <p>password:</p>
-          <input 
-            name="password"
-            type="text"
-            onChange={this.onChange}
-            value={this.state.password}
-          />
-          <button type="submit">connect</button>
-        </form>
-      </div>
-    );
+      <Dialog open>
+        <DialogTitle>Enter Server URL to login</DialogTitle>
+        <div className="login-modal">
+          <form onSubmit={this.onSubmit}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="url"
+              label="URL"
+              autoFocus
+              defaultValue={this.state.url}
+              onChange = {this.onChange}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Username"
+              defaultValue={this.state.username}
+              onChange = {this.onChange}
+            />
+            <br/>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              label="Password"
+              type="password"
+              id="password"
+              defaultValue={this.state.password}
+              onChange = {this.onChange}
+            />
+            <br/>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+            >
+              LOGIN
+            </Button>
+          </form>
+        </div>
+      </Dialog>
+    )
   }
 }
 
